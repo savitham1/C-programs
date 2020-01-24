57fec3f8902fc8bb2341bb4c56c9e12ed0f910f8
savitha1 <59514085+savitham1@users.noreply.github.com>
2020-01-24 17:17:37 -0500

    Update rand0.c

--- rand0.c
+++ rand0.c
@@ -13,7 +13,7 @@ Savitha Munirajaiah
 
 #include<stdio.h>
 
-static unsigned long int seed = 1;
+unsigned long int seed;
 extern void srand(unsigned int prev);
 
 unsigned int rand0()
@@ -21,7 +21,7 @@ unsigned int rand0()
     // Magic formulae
     // Linear Congruential Generator Algorithm
     seed = seed * 1103515245 + 12345;
-    return (unsigned int) (seed/ 65536) % 32768;
+    return (unsigned int)   (seed/ 65536) % 32768;
 
 }
 
