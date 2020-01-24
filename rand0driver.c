7e8369587a38239c608113ec159a1ac58e9d3945
savitha1 <59514085+savitham1@users.noreply.github.com>
2020-01-24 17:17:08 -0500

    Update rand0driver.c

--- rand0driver.c
+++ rand0driver.c
@@ -4,30 +4,33 @@ run with rand0.c
 */
 
 #include<stdio.h>
-#include<string.h>
 
-extern unsigned int rand0();
-extern void srand0(unsigned int);
-extern unsigned long int seed;
+extern unsigned int rand0(); // external linkage; file scope function
+extern void srand0(unsigned int); 
+extern unsigned long int seed; // external linkage; file scope; static storage duration
 
 int main(void)
 {
-    //extern unsigned int prevnum
+   
     int count = 0;
     unsigned int num = 0;
     printf("Welcome to Random Number Generator!\n");
     printf("How many random number would you like? \n");
     scanf("%d", &count);
 
+    // Get the initial seed value from the user, this increases the randomness of the program
+    // Alternatively, this seed value/ object can be initialized to system time.
+    
     printf("Kindly choose a seed value:\n");
     scanf("%ld", &seed);
 
+    // Iterate throught the total numbers requested by the user
     for(int i = 1; i <= count; i++)
     {
         num = rand0();
+        // To store the previous seed value
         srand0(num);
         printf("\n%d: %d", i, num);
     }
-
     return 0;
 }
