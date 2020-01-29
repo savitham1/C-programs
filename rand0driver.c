292368d8845391eff251e81d773b59959a83099f
savitha1 <59514085+savitham1@users.noreply.github.com>
2020-01-29 16:04:11 -0500

    Update rand0driver.c
    Additional comments included

--- rand0driver.c
+++ rand0driver.c
@@ -1,36 +1,45 @@
 /* Random number generator
+
 rand0driver.c -- driver program for rand0.c
+
 run with rand0.c
+
+Savitha Munirajaiah
+23rd January, 2020.
+
 */
 
 #include<stdio.h>
+#include<string.h>
 
-extern unsigned int rand0(); // external linkage; file scope function
-extern void srand0(unsigned int); 
-extern unsigned long int seed; // external linkage; file scope; static storage duration
+extern unsigned int rand0();
+extern void srand0(unsigned int);
+extern unsigned long int seed;
 
 int main(void)
 {
-   
+    // Stores the number of random numbers generated every call
     int count = 0;
+
+    // Stores the random number to be printed
     unsigned int num = 0;
+
     printf("Welcome to Random Number Generator!\n");
     printf("How many random number would you like? \n");
     scanf("%d", &count);
 
-    // Get the initial seed value from the user, this increases the randomness of the program
-    // Alternatively, this seed value/ object can be initialized to system time.
-    
-    printf("Kindly choose a seed value:\n");
-    scanf("%ld", &seed);
+    // User initializing the seed value
 
-    // Iterate throught the total numbers requested by the user
-    for(int i = 1; i <= count; i++)
+    printf("Kindly choose a seed value:\n");
+    while(scanf("%ld", &seed) == 1)
     {
-        num = rand0();
-        // To store the previous seed value
-        srand0(num);
-        printf("\n%d: %d", i, num);
+        for(int i = 1; i <= count; i++)
+        {
+            num = rand0();
+            printf("\n%d: %d", i, num);
+            srand0(seed);
+        }
+        printf("\nPlease enter the next seed or 'q' to quit\n");
     }
     return 0;
 }
